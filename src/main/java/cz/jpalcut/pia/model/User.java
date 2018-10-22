f983d998341295b381ad6458e4fe811557fe7ec6
hard456 <janpalcut@seznam.cz>
2018-10-22 16:18:23 +0200

    user model

---
+++ src/main/java/cz/jpalcut/pia/model/User.java
@@ -0,0 +1,95 @@
+package cz.jpalcut.pia.model;
+
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(schema = "public", name = "user")
+public class User implements Serializable{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "login_id")
+    private Integer loginId;
+
+    @Column(name = "pin")
+    private String pin;
+
+    @ManyToMany
+    @JoinTable(
+            name="user_role",
+            joinColumns=@JoinColumn(name="user_id", referencedColumnName="id"),
+            inverseJoinColumns=@JoinColumn(name="role_id", referencedColumnName="id"))
+    private List<Role> roleList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(Integer loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public List<Role> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<Role> roleList) {
+        this.roleList = roleList;
+    }
+}
