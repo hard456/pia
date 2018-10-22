3fe07e9cd7fb1e6528f75f94ce94c42c29eba65c
hard456 <janpalcut@seznam.cz>
2018-10-22 16:28:54 +0200

    model role

---
+++ src/main/java/cz/jpalcut/pia/model/Role.java
@@ -0,0 +1,37 @@
+package cz.jpalcut.pia.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy="roleList")
+    private List<User> users;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
