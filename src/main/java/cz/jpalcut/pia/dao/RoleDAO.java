d2599debab394751d62741e7a3e1bdbfcedc8529
hard456 <janpalcut@seznam.cz>
2018-12-29 21:37:13 +0100

    DAO update

--- src/main/java/cz/jpalcut/pia/dao/RoleDAO.java
+++ src/main/java/cz/jpalcut/pia/dao/RoleDAO.java
@@ -1,12 +1,10 @@
 package cz.jpalcut.pia.dao;
 
-import cz.jpalcut.pia.model.User;
+import cz.jpalcut.pia.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDAO extends JpaRepository<User, Long> {
-
-    User findUserByLoginId(String loginId);
+public interface RoleDAO extends JpaRepository<Role, Long> {
 
 }
--- src/main/java/cz/jpalcut/pia/dao/TransactionDAO.java
+++ src/main/java/cz/jpalcut/pia/dao/TransactionDAO.java
@@ -1,12 +1,10 @@
 package cz.jpalcut.pia.dao;
 
-import cz.jpalcut.pia.model.User;
+import cz.jpalcut.pia.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDAO extends JpaRepository<User, Long> {
-
-    User findUserByLoginId(String loginId);
+public interface TransactionDAO extends JpaRepository<Transaction, Long> {
 
 }
