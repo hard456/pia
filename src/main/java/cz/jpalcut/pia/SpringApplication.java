c39662ed995fdb343432eeb49d843f6ab25d57e8
hard456 <janpalcut@seznam.cz>
2019-01-05 01:34:00 +0100

    komentáře a formátování textu

--- src/main/java/cz/jpalcut/pia/SpringApplication.java
+++ src/main/java/cz/jpalcut/pia/SpringApplication.java
@@ -5,18 +5,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringApplication extends SpringBootServletInitializer
-{
+public class SpringApplication extends SpringBootServletInitializer {
 
     @Override
-    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SpringApplication.class);
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         org.springframework.boot.SpringApplication app =
-                  new org.springframework.boot.SpringApplication(SpringApplication.class);
+                new org.springframework.boot.SpringApplication(SpringApplication.class);
         app.run(args);
     }
 }
